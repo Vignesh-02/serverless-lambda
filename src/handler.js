d83ea68487d08872d8885f627a8b24cd30a86682
Vignesh <vignesh@turnkey.work>
2026-01-13 12:56:36 +0530

    added proper error handling

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
-module.exports.generateRandomNumber =  (event) => {
-  const num = parseInt(Math.random() * 10);
-//   Adding number 7 to it
-  const out = num + 7;
-  return out;
+module.exports.generateRandomNumber = (event) => {
+    const response = { statusCode: 200 };
+    try {
+        const num = parseInt(Math.random() * 10);
+        //   Adding number 7 to it
+        const out = num + 7;
+
+        response.body = JSON.stringify({
+            message: "Successfully generated a random lucky number",
+            number: out,
+        });
+    } catch (err) {
+        console.log(err);
+        response.statusCode = 500
+        response.body = JSON.stringify({
+            message: "Failed to generate random number",
+            error: err.message,
+            errorStack: err.stack
+        });
+    }
+    return response;
 };
